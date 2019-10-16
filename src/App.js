2af258b5135099621611f9a8ef5e1fadcb9298f9
Roland Enola <40168844+mystiexx@users.noreply.github.com>
2019-10-17 00:11:42 +0100

    about page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, {Component } from 'react';
 import './App.css';
 import Home from './components/HomeTab/Home';
-import Header from './components/HeaderTab/Header';
 
 class App extends  Component{
   render(){
     return(
-      <div className="body">
-        <Header/>
+      <div>
         <Home/>
       </div>
     )
